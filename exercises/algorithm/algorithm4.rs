fdc300494f89a6b625d7bd9a2fff5b62f2d27d38
leauny <leauny@outlook.com>
2025-09-23 00:33:08 +0800

    feat: algorithms

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
@@ -41,7 +40,7 @@ where
 
 impl<T> BinarySearchTree<T>
 where
-    T: Ord,
+    T: Ord + Debug,
 {
 
     fn new() -> Self {
@@ -50,13 +49,27 @@ where
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+        match self.root {
+            None => {
+                self.root = Some(Box::new(TreeNode::new(value)));
+            }
+            Some(ref mut node) => {
+                node.insert(value);
+            }
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        let mut curr = &self.root;
+        while let Some(node) = curr {
+            match node.value.cmp(&value) {
+                Ordering::Equal => return true,
+                Ordering::Greater => curr = &node.left,
+                Ordering::Less => curr = &node.right,
+            }
+        }
+        false
     }
 }
 
@@ -66,7 +79,25 @@ where
 {
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
-        //TODO
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                if let Some(ref mut left) = self.left {
+                    left.insert(value);
+                } else {
+                    self.left = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Greater => {
+                if let Some(ref mut right) = self.right {
+                    right.insert(value);
+                } else {
+                    self.right = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Equal => {
+                
+            }
+        }
     }
 }
 
@@ -89,6 +120,7 @@ mod tests {
         bst.insert(2);
         bst.insert(4);
 
+        println!("BST: {:?}", bst);
         
         assert_eq!(bst.search(5), true);
         assert_eq!(bst.search(3), true);
